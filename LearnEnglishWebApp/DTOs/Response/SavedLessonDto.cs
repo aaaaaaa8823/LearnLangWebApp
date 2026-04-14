e0a1bf785f10a16d7927593b052f4c558b4b2269
unknown <y_kumarova@list.ru>
2026-04-14 18:04:42 +0400

    add DTO's for saving lesson and test

---
+++ LearnEnglishWebApp/DTOs/Response/SavedLessonDto.cs
@@ -0,0 +1,13 @@
+﻿namespace LearnEnglishWebApp.DTOs.Response
+{
+    public class SavedLessonDto
+    {
+        public long Id { get; set; }
+        public long LessonId { get; set; }
+        public string Title { get; set; }
+        public string Level { get; set; }
+        public string Description { get; set; }
+        public string LessonType { get; set; } 
+        public DateTime SavedAt { get; set; }
+    }
+}
---
+++ LearnEnglishWebApp/DTOs/Response/SavedTestDto.cs
@@ -0,0 +1,14 @@
+﻿namespace LearnEnglishWebApp.DTOs.Response
+{
+    public class SavedTestDto
+    {
+        public long Id { get; set; }
+        public long TestId { get; set; }
+        public string Title { get; set; }
+        public string Level { get; set; }
+        public string Description { get; set; }
+        public string TestType { get; set; } 
+        public int QuestionCount { get; set; }
+        public DateTime SavedAt { get; set; }
+    }
+}
