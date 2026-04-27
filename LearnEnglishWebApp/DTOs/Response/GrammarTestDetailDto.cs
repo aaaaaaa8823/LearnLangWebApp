b4420e23e8ba26f62f80ec712778bb3b63e5e904
unknown <y_kumarova@list.ru>
2026-04-27 18:18:57 +0400

    add DTO for grammar tests

---
+++ LearnEnglishWebApp/DTOs/Response/GrammarTestDetailDto.cs
@@ -0,0 +1,42 @@
+﻿namespace LearnEnglishWebApp.DTOs.Response
+{
+    public class GrammarTestDetailDto
+    {
+        public long Id { get; set; }
+        public long GrammarTopicId {  get; set; }
+        public string TopicTitle {  get; set; }
+        public string Level {  get; set; }
+        public int QuestionCount { get; set; }
+        public int PassingScore {  get; set; }
+        public int TimeLimitMinutes { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsCompleted { get; set; }
+        public int? BestScore { get; set; }
+        public double? BestPercentage { get; set; }
+        public int AttemptCount { get; set; }
+
+    }
+
+    public class SubmitTestResultDto
+    {
+        public long TestId {  get; set; }
+        public int Score { get; set; }
+        public int MaxScore {  get; set; }
+        public double Percentage { get; set; }
+        public int TimeSpentSeconds { get; set; }
+        public int CorrectAnswers { get; set; }
+        public int WrongAnswers { get; set; }
+        public List<TestAnswerDetailDto> Answers { get; set; }
+    }
+
+    public class TestAnswerDetailDto
+    {
+        public int QuestionId { get; set; }
+        public string UserAnswer { get; set; }
+        public string CorrectAnswer { get; set; }
+        public bool IsCorrect { get; set; }
+        public int PointsEarned { get; set; }
+        public int MaxPoints { get; set; }
+    }
+}
+
