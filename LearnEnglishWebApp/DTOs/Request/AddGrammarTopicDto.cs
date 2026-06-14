0254e95d71d63112daf2ca6c37a44acb5eaa38ed
unknown <y_kumarova@list.ru>
2026-06-14 17:06:38 +0400

    update grammar and vocab admin dto

--- LearnEnglishWebApp/DTOs/Request/AddGrammarTopicDto.cs
+++ LearnEnglishWebApp/DTOs/Request/AddGrammarTopicDto.cs
@@ -4,12 +4,12 @@
     {
         public string Level { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int OrderIndex { get; set; }
 
-        public string TheoryContent { get; set; }
+        public string? TheoryContent { get; set; }
 
-        public string ExamplesContent { get; set; }
+        public string? ExamplesContent { get; set; }
     }
 
     public class UpdateGrammarTopicDto : AddGrammarTopicDto { }
--- LearnEnglishWebApp/DTOs/Request/AddVocabLessonDto.cs
+++ LearnEnglishWebApp/DTOs/Request/AddVocabLessonDto.cs
@@ -4,10 +4,10 @@
     {
         public string Level { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int OrderIndex { get; set; }
-        public string TheoryContent { get; set; }
-        public string ExamplesContent { get; set; }
+        public string? TheoryContent { get; set; }
+        public string? ExamplesContent { get; set; }
     }
     public class UpdateVocabLessonDto : AddVocabLessonDto { }
 }
