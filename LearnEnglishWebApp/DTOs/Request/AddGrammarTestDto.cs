693f485f9b96044d563da76db3564d2c4f0eb399
unknown <y_kumarova@list.ru>
2026-06-15 13:10:44 +0400

    update grammar test dto

--- LearnEnglishWebApp/DTOs/Request/AddGrammarTestDto.cs
+++ LearnEnglishWebApp/DTOs/Request/AddGrammarTestDto.cs
@@ -9,6 +9,8 @@
         public int PassingScore { get; set; }
         public int TimeLimitMinutes { get; set; }
         public int OrderIndex { get; set; }
+        public string? QuestionsText { get; set; }   
+        public string? AnswersText { get; set; }
     }
 
     public class UpdateGrammarTestDto : AddGrammarTestDto { }
---
+++ LearnEnglishWebApp/DTOs/Response/GrammarTestAdminDto.cs
@@ -0,0 +1,26 @@
+﻿namespace LearnEnglishWebApp.DTOs.Response
+{
+    public class GrammarTestAdminDto
+    {
+        public long Id { get; set; }
+        public long GrammarTopicId { get; set; }
+        public string Title { get; set; }
+        public string Level { get; set; }
+        public int QuestionCount { get; set; }
+        public int PassingScore { get; set; }
+        public int TimeLimitMinutes { get; set; }
+        public int OrderIndex { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? QuestionsText { get; set; }
+        public string? AnswersText { get; set; }
+        public GrammarTopicSimpleDto? GrammarTopic { get; set; }
+    }
+
+    public class GrammarTopicSimpleDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Level { get; set; }
+    }
+}
